eb545d9933ee8c20d719f66fec93d94ddea2a427
Nico Flaig <nfla@camelot-itlab.com>
2019-12-27 22:25:29 +0100

    Initial prototype

---
+++ bin/github-init-cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const {setCredentials, setWorkingDir, create} = require('../lib/github-init-cli');
+
+const args = process.argv.splice(process.execArgv.length + 2);
+
+const command = args[0];
+
+if(command === "setCredentials") setCredentials(args[1], args[2]);
+else if(command === "setWorkingDir") setWorkingDir(args[1]);
+else if (command === "create") create(args[1]);
+else console.log("Unknown command");
